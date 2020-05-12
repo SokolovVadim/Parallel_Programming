efbf0a608a9c2583ef3078cd9caa20e3b26eebe0
Vadim <varem99@mail.ru>
2020-05-12 11:44:24 +0300

    new delete

--- 5_Static_balancing/main.cpp
+++ 5_Static_balancing/main.cpp
@@ -1,62 +1,9 @@
-/*#include <iostream>
-#include <mpi.h>
-#include <stdexcept>
-#include <climits>
-#include <cmath>
-
-typedef enum {
-	OK = 0,
-	FAIL = 1
-} status_t;
-
-typedef struct {
-	int start;
-	int end;
-} task_t;
-
-// -----------------------------------------------------------------------------------------------------
-
-int main(int argc, char* argv[])
-{
-	int proc_num(0), proc_rank(0);
-
-	try{
-
-	// MPI code starts here
-
-	int error = MPI_Init(&argc, &argv);
-	if(error)
-		std::cerr << "MPI_Init crashed" << std::endl;
-
-	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
-	MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
-
-	long int length(0);
-	if(proc_rank == 0)
-	{
-		if(argc != 2)
-			throw std::runtime_error("Invalid number of arguments!");
-		
-	}
-	
-	MPI_Finalize();
-
-	// MPI code ends here
-
-	} catch(std::runtime_error & err){
-		std::cout << err.what() << std::endl;
-	}
-}
-
-// -----------------------------------------------------------------------------------------------------
-
-*/
-
 #include <iostream>
 #include <mpi.h>
 #include <unistd.h>
 #include <malloc.h>
 #include <climits>
+#include <fstream>
 
 typedef enum {
   OK = 0,
@@ -69,7 +16,7 @@ typedef struct {
 } task_t;
 
 long int ReadArg(char * str);
-int RootRoutine(int size, int tasksNumber, status_t status, task_t & localTask, double & startTime);
+void* RootRoutine(int size, int tasksNumber, status_t status, task_t & localTask, double & startTime);
 int SlaveRoutine(int rank);
 
 int main(int argc, char* argv[])
@@ -82,6 +29,8 @@ int main(int argc, char* argv[])
   int size = 0;
   double startTime = 0;
   double endTime = 0;
+  task_t* taskShedule(nullptr);
+  std::ifstream fin;
   int error = MPI_Init(&argc, &argv);
   if(error)
 	std::cerr << "MPI_Init crashed" << std::endl;
@@ -95,7 +44,7 @@ int main(int argc, char* argv[])
     // Root branch
     //
     // Check arguments
-    if (argc != 2 || (tasksNumber = ReadArg(argv[1])) < 1)
+    if (argc != 4 || (tasksNumber = ReadArg(argv[1])) < 1)
     {
       printf("Usage:\n required 1 argument (> 0) for tasks number\n");
       // Exit with fail status
@@ -104,6 +53,7 @@ int main(int argc, char* argv[])
       MPI_Finalize();
       return 0;
     }
+    fin.open(argv[2]);
     RootRoutine(size, tasksNumber, status, localTask, startTime);
   }
   else
@@ -118,17 +68,17 @@ int main(int argc, char* argv[])
   {
     endTime = MPI_Wtime();
     printf("[TIME RES] %lf\n", endTime - startTime);
+    fin.close();
+    delete[] taskShedule;
   }
 
   MPI_Finalize();
   return 0;
 }
 
-int RootRoutine(int size, int tasksNumber, status_t status, task_t & localTask, double & startTime)
+void* RootRoutine(int size, int tasksNumber, status_t status, task_t & localTask, double & startTime)
 {
 	// Root branch
-
-    std::cout << size << " argv[1] = " << tasksNumber << std::endl;
     // Send status of arguments
     status = OK;
     MPI_Bcast(&status, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -138,11 +88,11 @@ int RootRoutine(int size, int tasksNumber, status_t status, task_t & localTask, 
     int localLen = tasksNumber / (size - 1);
     int rest = tasksNumber % (size - 1);
   
-    task_t* taskShedule = (task_t*)calloc(size, sizeof(task_t));
+    task_t* taskShedule = new task_t[size];
     if(taskShedule == nullptr)
     {
     	std::cout << "memory allocation failed!" << std::endl;
-    	return 0;
+    	return NULL;
     }
 
     int taskPointer = 0;
@@ -165,7 +115,7 @@ int RootRoutine(int size, int tasksNumber, status_t status, task_t & localTask, 
     // Send tasks for work begin
     startTime = MPI_Wtime();
     MPI_Scatter(taskShedule, 2, MPI_INT, &localTask, 2, MPI_INT, 0, MPI_COMM_WORLD);
-    return 0;
+    return taskShedule;
 }
 
 int SlaveRoutine(int rank)
