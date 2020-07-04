30003343aae3b8795cc9a4c48eae50a697339efd
Vadim <varem99@mail.ru>
2020-07-04 16:50:14 +0300

    one worker can perform multple jobs

--- 6_Dynamic_balancing/main.cpp
+++ 6_Dynamic_balancing/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
 #include <mpi.h>
 
 enum TOKEN
@@ -30,7 +31,7 @@ struct Numbers{
 
 int ReadNumbers(char* in, int size, Numbers** numbers_);
 void RootRoutine(int size, Numbers* numbers, int token_number);
-void ClientRoutine();
+void ClientRoutine(int rank);
 	
 int main(int argc, char* argv[])
 {
@@ -70,7 +71,7 @@ int main(int argc, char* argv[])
 			MPI_Finalize();
 			return EXIT_FAILURE;
 		}
-		ClientRoutine();
+		ClientRoutine(rank);
 	}
 
 	MPI_Finalize();
@@ -79,23 +80,59 @@ int main(int argc, char* argv[])
 
 void RootRoutine(int size, Numbers* numbers, int token_number)
 {
-	
-	for(int i(1); i < token_number; ++i)
+	//int process_worked(0);
+	int number_of_iterations = token_number / (size - 1);
+	std::cout << "token_number = " << token_number << " size = " << size << std::endl;
+	int i(0);
+	for(i = 1; i < size; ++i)
 	{
-		// MPI_Send(&token_number, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-    	MPI_Send(&numbers[i - 1].first,  1, MPI_INT, i, 0, MPI_COMM_WORLD);
-    	MPI_Send(&numbers[i - 1].second, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-    	// MPI_Send(numbers[i - 1].digit_transfer, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&number_of_iterations,  1, MPI_INT, i, OK, MPI_COMM_WORLD);
+		MPI_Send(&numbers[i - 1].first,  1, MPI_INT, i, OK, MPI_COMM_WORLD);
+    	MPI_Send(&numbers[i - 1].second, 1, MPI_INT, i, OK, MPI_COMM_WORLD);
 	}
-
+	while(number_of_iterations > 1)
+	{
+		for(i = 1; i < size; ++i)
+		{
+			std::cout << "process " << i << " started it's job\n";
+			MPI_Send(&number_of_iterations,  1, MPI_INT, i, OK, MPI_COMM_WORLD);
+			MPI_Send(&numbers[i - 1].first,  1, MPI_INT, i, OK, MPI_COMM_WORLD);
+	    	MPI_Send(&numbers[i - 1].second, 1, MPI_INT, i, OK, MPI_COMM_WORLD);
+		}
+    	number_of_iterations--;
+	}
+	/*int status(0);
+	do
+	{
+		status = (process_worked + 1 >= size ? FAIL: OK);
+		std::cout << "process " << process_worked + 1 << " started it's job with status "
+		 << status << " pw + 1 % size = " << (process_worked + 1) % size << std::endl;
+		MPI_Send(&numbers[process_worked].first,  1, MPI_INT, (process_worked + 1) % size, status, MPI_COMM_WORLD);
+    	MPI_Send(&numbers[process_worked].second, 1, MPI_INT, (process_worked + 1) % size, status, MPI_COMM_WORLD);
+    	process_worked++;
+	} while(process_worked != token_number);*/
 }
 
-void ClientRoutine()
+void ClientRoutine(int rank)
 {
 	MPI_Status mpi_status = {};
 	int first(0), second(0);//, digit_transfer(0);
-	MPI_Recv(&first, 1,  MPI_INT, ROOT, 0, MPI_COMM_WORLD, &mpi_status);
-	MPI_Recv(&second, 1,  MPI_INT, ROOT, 0, MPI_COMM_WORLD, &mpi_status);
+	int counter(0);
+	int number_of_iterations(0);
+	MPI_Recv(&number_of_iterations, 1,  MPI_INT, ROOT, 0, MPI_COMM_WORLD, &mpi_status);
+	while(number_of_iterations != 0)
+	{
+		MPI_Recv(&first, 1,  MPI_INT, ROOT, 0, MPI_COMM_WORLD, &mpi_status);
+		MPI_Recv(&second, 1,  MPI_INT, ROOT, 0, MPI_COMM_WORLD, &mpi_status);
+		std::cout << "process " << rank << std::endl;
+/*		if(mpi_status.MPI_SOURCE == FAIL)
+		{
+			std::cout << counter;
+			break;
+		}*/
+		counter++;
+		number_of_iterations--;
+	}
 	//MPI_Recv(digit_transfer, 1,  MPI_INT, ROOT, 0, MPI_COMM_WORLD, &mpi_status);
 }
 
