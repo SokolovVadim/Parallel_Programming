7e3b9ab64dcd686765f166fb5f5af8e9dcd7f1f5
Vadim <varem99@mail.ru>
2020-07-06 17:34:15 +0300

    final version with visual changes

--- 6_Dynamic_balancing/make_test.cpp
+++ 6_Dynamic_balancing/make_test.cpp
@@ -4,11 +4,11 @@
 int main()
 {
 	std::ofstream fout("input.txt");
-	int N = 9 * 40000;
+	int N = 9 * 16;
 	fout << N << std::endl;
 	for(int i(0); i < N; ++i)
 	{
-		fout << 1;
+		fout << 9;
 	}
 	fout << std::endl;
 	for(int i(0); i < N; ++i)
