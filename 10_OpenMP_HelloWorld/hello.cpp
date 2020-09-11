a089f1d71f16f9b0837dc837bf7b710de8074098
Vadim <varem99@mail.ru>
2020-09-11 15:09:52 +0300

    set num threads

--- 10_OpenMP_HelloWorld/hello.cpp
+++ 10_OpenMP_HelloWorld/hello.cpp
@@ -2,13 +2,22 @@
 #include <string>
 #include <omp.h>
 
+enum NUMBERS
+{
+	CORE_NUM = 4,
+	THREAD_NUM = 8,
+	CUR_THREAD_NUM = 16
+};
+
 int main()
 {
 	std::cout << "Hello!\n";
 
+	omp_set_num_threads(THREAD_NUM);
+
 	// Parallel code starts here
 
-	#pragma omp parallel
+	#pragma omp parallel num_threads(CUR_THREAD_NUM)
 	{
 		std::string msg = "Hello from process ";
 		msg += std::to_string(omp_get_thread_num()) + "\n";
