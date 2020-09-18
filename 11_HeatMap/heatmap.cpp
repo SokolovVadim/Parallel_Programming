1242e6be1e965f522b569f87e96a6703656fca18
Vadim <varem99@mail.ru>
2020-09-18 18:08:39 +0300

    heatmap script

--- 11_HeatMap/heatmap.cpp
+++ 11_HeatMap/heatmap.cpp
@@ -2,20 +2,55 @@
 #include <string>
 #include <omp.h>
 #include <sys/time.h>
+#include <climits>
 
 enum NUMBERS
 {
 	CORE_NUM = 4,
 	THREAD_NUM = 8,
 	INTERNAL_THREAD_NUM = 16,
-	FOR_MAX = 100
+	FOR_MAX = 100,
+	COUNTER_MAX = 10000
 };
 
-void process_routine()
+long int ReadArg(char * str);
+void create_threads(int thread_num);
+void process_routine();
+
+int main(int argc, char* argv[])
 {
-	int counter(0);
-	for(int i(0); i < FOR_MAX; ++i)
-		counter++;
+	if(argc != 3)
+	{
+		std::cout << "Program requires two input parameters!\n";
+		exit(EXIT_FAILURE);
+	}
+	int external_thread_num = ReadArg(argv[1]);
+	int internal_thread_num = ReadArg(argv[2]);
+	omp_set_nested(true);
+	omp_set_num_threads(THREAD_NUM);
+
+	double start = omp_get_wtime();
+
+	// Parallel code starts here
+
+	#pragma omp parallel num_threads(external_thread_num)
+	{
+		for(int i(0); i < FOR_MAX; ++i)
+		{
+			// std::string msg = "External process " + std::to_string(omp_get_thread_num()) + "\n";
+			create_threads(internal_thread_num);
+			// std::cout << msg;
+		}
+	}
+
+	// Parallel code ends here
+
+	double end = omp_get_wtime();
+
+	double delta = end - start;
+	std::cout << delta << std::endl;
+
+	return 0;
 }
 
 void create_threads(int thread_num)
@@ -31,31 +66,38 @@ void create_threads(int thread_num)
 	}
 }
 
-int main(int argc, char* argv[])
+void process_routine()
 {
-	omp_set_nested(true);
-	omp_set_num_threads(THREAD_NUM);
+	int counter(0);
+	for(int i(0); i < COUNTER_MAX; ++i)
+		counter++;
+}
 
-	double start = omp_get_wtime();
 
-	// Parallel code starts here
+long int ReadArg(char * str)
+{
+	char* endptr;
+	errno = 0;
 
-	#pragma omp parallel num_threads(CORE_NUM)
+	long int number = strtol(str, &endptr, 10);
+
+	
+	if ((errno == ERANGE && (number == LONG_MAX || number == LONG_MIN)) || (errno != 0 && number == 0)) 
 	{
-		for(int i(0); i < FOR_MAX; ++i)
-		{
-			std::string msg = "External process " + std::to_string(omp_get_thread_num()) + "\n";
-			create_threads(INTERNAL_THREAD_NUM);
-			std::cout << msg;
-		}
-	}
+       		perror("strtol");
+        	exit(EXIT_FAILURE);
+   	}
 
-	// Parallel code ends here
+	if (endptr == str)
+	{
+        	fprintf(stderr, "Error!\n");
+        	exit(EXIT_FAILURE);
+   	}
+	if (*endptr != '\0')
+	{
+        	fprintf(stderr, "Error!\n");
+        	exit(EXIT_FAILURE);
+   	}
 
-	double end = omp_get_wtime();
-
-	double delta = end - start;
-	std::cout << delta << std::endl;
-
-	return 0;
+	return number;
 }
--- 11_HeatMap/script.py
+++ 11_HeatMap/script.py
@@ -3,18 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def func(external_proc_num, internal_proc_num, Matrix):
-	for e in range(external_proc_num):
-		for i in range(internal_proc_num):
+	for e in range(0, external_proc_num):
+		for i in range(0, internal_proc_num):
 			cmd = './heatmap.out ' + str(e) + ' ' + str(i) 
-			print(cmd, sep = '\n')
+			# print(cmd, sep = '\n')
 			so = os.popen(cmd).read()
 			# print(so)
 			Matrix[e][i] = so
+def draw(Matrix):
+	plt.imshow(Matrix, cmap='hot', interpolation='nearest')
+	plt.show()
 
 if __name__ == "__main__":
-	ex_proc_num = 2
-	in_proc_num = 4
+	ex_proc_num = 4
+	in_proc_num = 16
 	Matrix = np.zeros((ex_proc_num, in_proc_num))
-	print(Matrix)
+	
 	func(ex_proc_num, in_proc_num, Matrix)
+
 	print(Matrix)
+	draw(Matrix)
